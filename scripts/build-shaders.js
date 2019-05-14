6360a1bf5415c69db1251702a1e7ea4b6dc62456
Rich Harris <richard.a.harris@gmail.com>
2019-05-14 19:50:00 -0400

    remove build-shaders script

--- scripts/build-shaders.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const glob = require('tiny-glob/sync');
-
-glob('**/*.glsl').forEach(file => {
-	const src = fs.readFileSync(file, 'utf-8');
-	const js = `export default ${JSON.stringify(src)};`;
-	fs.writeFileSync(file + '.mjs', js);
-});
