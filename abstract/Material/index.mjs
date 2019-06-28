29455177fbd190b1555ec1af7aa98032a164c472
Rich Harris <richard.a.harris@gmail.com>
2019-06-28 11:29:47 -0400

    clamp to edge

--- abstract/Material/index.mjs
+++ abstract/Material/index.mjs
@@ -62,6 +62,9 @@ export default class Material {
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+
 		this._textures[id] = texture;
 
 		this._update_hash();
